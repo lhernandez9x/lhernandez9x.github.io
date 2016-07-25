f9b42c1792dd157698b6c306e10ad7614fc106bc
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-07-25 17:03:27 -0600

    feat: Add skills section and change styles

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ $('.menu-icon').click(function(){
     $('.menu-list').toggleClass('menu-open');
 })
 
+
+// Toggle Service Content
+var processContainer = $('.process-container'),
+    processContent = $('.process-container p');
+
+    console.log(processContent);
