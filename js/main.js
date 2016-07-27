8a87bc51fb78c2f6625c409c292188e423e6d2f9
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-07-27 13:45:20 -0600

    feat: Add click and mouseover event handler on services items.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
+function isMobile () {
+  if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i) || navigator.userAgent.match(/Opera Mini/i) || navigator.userAgent.match(/IEMobile/i)) {
+    return true
+  }
+}
+
 // menu toggle
 
-$('.menu-icon').click(function(){
-    $('.menu-list').toggleClass('menu-open');
+$('.menu-icon').click(function () {
+  $('.menu-list').toggleClass('menu-open')
 })
 
+var processContainer = $('.process-container')
 
-// Toggle Service Content
-var processContainer = $('.process-container'),
-    processContent = $('.process-container p');
+// Toggle class on clickt
+if (isMobile()) {
+  processContainer.click(function () {
+    var titleContent = $(this).children('div .title-content'),
+      bodyContent = $(this).children('div .body-content')
 
-    console.log(processContent);
+    titleContent.toggleClass('content-hidden')
+    bodyContent.toggleClass('content-hidden')
+  })
+} else {
+  // toggle class on mouse enter
+  processContainer.mouseenter(function () {
+    var titleContent = $(this).children('div .title-content'),
+      bodyContent = $(this).children('div .body-content')
+
+    titleContent.toggleClass('content-hidden')
+    bodyContent.toggleClass('content-hidden')
+  })
+
+  // toggle class on mouse leave
+  processContainer.mouseleave(function () {
+    var titleContent = $(this).children('div .title-content'),
+      bodyContent = $(this).children('div .body-content')
+
+    titleContent.toggleClass('content-hidden')
+    bodyContent.toggleClass('content-hidden')
+  })
+}
