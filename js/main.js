c5089c4414f9b051390d507b283a9d9e85785cdd
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-07-27 19:23:38 -0600

    fix: fix bug with service items

--- js/main.js
+++ js/main.js
@@ -1,42 +1,38 @@
+$(document).ready(function(){
+    "use strict";
+
 function isMobile () {
   if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i) || navigator.userAgent.match(/Opera Mini/i) || navigator.userAgent.match(/IEMobile/i)) {
-    return true
+    return true;
   }
 }
 
 // menu toggle
 
 $('.menu-icon').click(function () {
-  $('.menu-list').toggleClass('menu-open')
-})
+  $('.menu-list').toggleClass('menu-open');
+});
 
-var processContainer = $('.process-container')
+var processContainer = $('.process-container');
 
-// Toggle class on clickt
-if (isMobile()) {
-  processContainer.click(function () {
-    var titleContent = $(this).children('div .title-content'),
-      bodyContent = $(this).children('div .body-content')
+// Toggle class on click
+    console.log(processContainer.attr('style'));
+ 
+    processContainer.on('click', function () {
+        if ($(this).attr('style') == undefined || $(this).attr('style') == 'transform: rotateY(0deg);'){
+            $(this).css('transform','rotateY(360deg)');
+        } else {
+            $(this).css('transform','rotateY(0deg)');
+        }
+        var titleContent = $(this).children('div .title-content'),
+            bodyContent = $(this).children('div .body-content');
+     
+        //delay toggle class 
+        setTimeout(function(){
+            titleContent.toggleClass('content-hidden');
+            bodyContent.toggleClass('content-hidden');
+        },750);
+  });
 
-    titleContent.toggleClass('content-hidden')
-    bodyContent.toggleClass('content-hidden')
-  })
-} else {
-  // toggle class on mouse enter
-  processContainer.mouseenter(function () {
-    var titleContent = $(this).children('div .title-content'),
-      bodyContent = $(this).children('div .body-content')
-
-    titleContent.toggleClass('content-hidden')
-    bodyContent.toggleClass('content-hidden')
-  })
-
-  // toggle class on mouse leave
-  processContainer.mouseleave(function () {
-    var titleContent = $(this).children('div .title-content'),
-      bodyContent = $(this).children('div .body-content')
-
-    titleContent.toggleClass('content-hidden')
-    bodyContent.toggleClass('content-hidden')
-  })
-}
+// end 
+});
