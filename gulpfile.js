386fdc07489f7853943690958262094caab3355a
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-07-30 23:45:48 -0600

    fix: fix issue with overflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var uglyfly = require('gulp-uglyfly');
 var rename = require('gulp-rename');
@@ -13,23 +12,5 @@ gulp.task('default', function() {
 gulp.task('styles', function(){
     gulp.src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions']
-    }))
     .pipe(gulp.dest('css'));
-});
-
-gulp.task('compress', function(){
-    //CSS Minify
-    
-    gulp.src('css/*.css')
-    .pipe(cleanCSS({keepSpecialComments: '*'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css/'))
-    // JS Minify
-    
-    gulp.src('js/*.js')
-    .pipe(uglyfly({preserveComments: 'all'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('js/'))
 });
