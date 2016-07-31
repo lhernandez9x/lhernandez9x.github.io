c0ccc2777ed1637137454ef38e89e5e538485222
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-07-30 22:55:30 -0600

    fix: fix bug with scroll

--- js/main.js
+++ js/main.js
@@ -12,14 +12,16 @@ var dedicationSection = $('.dedication'),
     passionSection = $('.passion'),
     projectSection = $('.projects'),
     contactSection = $('.contact'),
-    moreButton = $('.more-button');
+    moreButton = $('.more-button'),
+    top = 0;
 
 moreButton.on('click', function(){
     var self = $(this),
         parent = self.parent(),
+        trueTop = top += window.innerHeight,
         body = parent.parent();
     // anitmate smooth scroll to next section     
-    body.animate({scrollTop: '+=640'}, 750);
+    body.animate({scrollTop: trueTop}, 750);
 
 })
 
