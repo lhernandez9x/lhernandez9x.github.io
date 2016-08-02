0104a5f8a624f49e31e8adc8a4270b313ae84d4a
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-08-01 22:35:55 -0600

    chore: beautifyand lint code

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
-$(document).ready(function(){
-    "use strict";
+$(document).ready(function() {
+    'use strict';
 
-function isMobile () {
-  if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i) || navigator.userAgent.match(/Opera Mini/i) || navigator.userAgent.match(/IEMobile/i)) {
-    return true;
-  }
-}
-
-//vars for smooth scrolling
-var moreButton = $('.more-button');
-
-moreButton.on('click', function() {
-    var self = $(this),
-        nextSection = self[0].firstChild.data,
-        top = window.innerHeight,
-        body = $('body');
-
-    // anitmate smooth scroll to next section
-    if(nextSection == 'Dedication') {
-        body.animate({scrollTop: top}, 750);
-    } else if(nextSection == 'Passion') {
-        body.animate({scrollTop: top * 2}, 750);
-    } else if(nextSection == 'Projects') {
-        body.animate({scrollTop: top * 3}, 750);
+    function isMobile() {
+        if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i) || navigator.userAgent.match(/Opera Mini/i) || navigator.userAgent.match(/IEMobile/i)) {
+            return true;
+        }
     }
-});
 
+    // vars for smooth scrolling
+    var moreButton = $('.more-button');
 
-// end 
+    moreButton.on('click', function() {
+        var self = $(this),
+            nextSection = self[0].firstChild.data,
+            top = window.innerHeight,
+            body = $('body');
+
+        // anitmate smooth scroll to next section
+        if (nextSection == 'Dedication') {
+            body.animate({
+                scrollTop: top
+            }, 750);
+        } else if (nextSection == 'Passion') {
+            body.animate({
+                scrollTop: top * 2
+            }, 750);
+        } else if (nextSection == 'Projects') {
+            body.animate({
+                scrollTop: top * 3
+            }, 750);
+        }
+    });
+
+    // end 
 });
